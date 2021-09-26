0a33257db4f6fbd98c208a1d1f2260cbeac96c10
Divya S Gupta <dg6219@srmist.edu.in>
2021-09-26 22:07:59 +0530

    Update indexjs.js
    updated

--- i_am_there/js/indexjs.js
+++ i_am_there/js/indexjs.js
@@ -4,8 +4,31 @@ const Open = document.getElementById('open');
 Open.addEventListener('click', () => {
     modalcontainer.classList.add('show');
 });
-window.onclick = function (event) {
-    if (event.target == modalcontainer) {
-        modalcontainer.classList.remove('show');
+
+function inactivityTime() {
+    let time;
+    window.onload = resetTime;
+    //window.onclick = resetTime;
+    window.onkeypress = resetTime;
+    window.ontouchstart = resetTime;
+    window.onmousemove = resetTime;
+    window.onmousedown = resetTime;
+    window.addEventListener('scroll', resetTime, true);
+
+    function alertUser() {
+        modalcontainer.classList.add('show');
     }
-}
+
+    function resetTime() {
+        clearTimeout(time);
+        time = setTimeout(alertUser, 1000 * 25); // 25 seconds
+    }
+    window.onclick = function (event) {
+        if (event.target == modalcontainer) {
+            modalcontainer.classList.remove('show');
+        }
+        resetTime();
+    };
+
+};
+
